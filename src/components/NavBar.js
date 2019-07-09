055dccdfd484ce0a666b349864771678efea0249
moises eastland <dewaynepade95@gmail.com>
2019-07-09 17:16:49 -0500

    changed Bootstrap.js to NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-function Bootstrap() {
+function NavBar() {
 return (
  <div className="container">
      <nav className= "navbar navbar-expand-lg navbar-light bg-light"></nav>
  </div>
 );
 }
-export default Bootstrap;
+export default NavBar;
