99d1efb1443da5509406a157e3241aa4591c552d
moises eastland <dewaynepade95@gmail.com>
2019-07-09 17:29:49 -0500

    created card,js and jumbotron.js files

---
+++ src/components/Cards.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Card() {
+    return(
+     <div className ="card">
+       <div className = "card-body">
+         <div className = "card-image">
+             
+         </div>
+       </div>
+     </div>
+
+    );
+}
---
+++ src/components/Jumbotron.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Jumbotron() {
+    return(
+        <div className = "jumbotron">
+            <h1></h1>
+        </div>
+
+    );
+}
+
+export default Jumbotron;
