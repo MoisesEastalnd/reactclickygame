dcddc66343557f26b2a9e6a9a6da9f4ae5f0f22d
moises eastland <dewaynepade95@gmail.com>
2019-07-09 17:31:35 -0500

    exported card.js file

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,10 +5,12 @@ function Card() {
      <div className ="card">
        <div className = "card-body">
          <div className = "card-image">
-             
+
          </div>
        </div>
      </div>
 
     );
 }
+
+export default Card;
