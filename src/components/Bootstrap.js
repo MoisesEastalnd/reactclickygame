50c7d7adcef4ba8ce6176a96ede702016ba21007
moises eastland <dewaynepade95@gmail.com>
2019-07-09 17:11:50 -0500

    creating bootstrap folder

---
+++ src/components/Bootstrap.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Bootstrap() {
+return (
+ <div className="container">
+     <nav className= "navbar navbar-expand-lg navbar-light bg-light"></nav>
+ </div>
+);
+}
+export default Bootstrap;
