9e0eb1ac06e87135941739a34481318a52184d19
moises eastland <dewaynepade95@gmail.com>
2019-07-10 22:09:08 -0500

    revamped navbar

--- src/App.js
+++ src/App.js
@@ -1,33 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Navbar from './components/NavbarLayout/Navbar';
 import './App.css';
-import { SSL_OP_DONT_INSERT_EMPTY_FRAGMENTS } from 'constants';
-import images from "./images.json";
 
 
+class App extends Component{
+  render() {
+    return (
+      <div className ='App'>
+       <Navbar />
+       </div>
+    );
+  }
   
-
-
-function App() {
-  console.log(images)
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
 }
 
 export default App;
---
+++ src/components/NavbarLayout/Navbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className='navbar bg-dark'>
+        <h1>
+        Navbar
+        </h1>
+      </nav>
+    );
+  }
+}
+
+export default Navbar
